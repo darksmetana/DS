6db86012a1753cf3192f077cbff7694fb2e2644e
darksmetana <darksmetana@gmail.com>
2015-09-15 06:22:32 +0800

    Update multiple.php

--- inc/multiple.php
+++ inc/multiple.php
@@ -1,1 +1,17 @@
-
+<form action="index.php?inc=multiple" method="POST">
+	<input type="text" name="rows">
+	<input type="text" name="colls">
+	<input type="submit" name="ok" value="Применить">
+</form>
+<table border='1'>
+<?php
+	if (isset($_POST['ok'])) {
+		for ($rows=1; $rows <= $_POST['rows']; $rows++) { 
+			for ($colls=1; $colls <= $_POST['colls']; $colls++) { 
+				echo "<td>" . ($rows*$colls) . "</td>";
+			}
+			if ($rows != $_POST['rows']) echo "<tr></tr>";
+		}
+	}
+?>
+</table>
