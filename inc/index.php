def4a615f3203331cd3f633dc096a9e515a38706
darksmetana <darksmetana@gmail.com>
2015-09-17 04:06:24 +0800

    Update index.php

--- inc/index.php
+++ inc/index.php
@@ -7,20 +7,23 @@
 </head>
 <body>
 <?php 
-	if (empty($_COOKIE['login']) and empty($_COOKIE['username'])) {
-	echo '<form action="index.php" method="POST" id="log"><input type="text" name="username" required placeholder="Set your name!"><br><input type="text" name="login" required placeholder="Set your login!"><br><input type="submit" name="enter" value="Log In"></form>';
+	if (empty($_COOKIE['login']) or empty($_COOKIE['username'])) {
+			echo '<form action="index.php" method="POST" id="log"><input type="text" name="username" required placeholder="Set your name!"><br><input type="text" name="login" required placeholder="Set your login!"><br><input type="submit" name="enter" value="Log In"></form>';
 	}
-	if (isset($_POST['login']) and isset($_POST['username'])) {
+	if (isset($_POST['login']) or isset($_POST['username'])) {
 			$username = $_POST['username'];
 			$login = $_POST['login'];
 			setcookie("login", "$login", time() + 3600, "/");
 			setcookie("username", "$username", time() + 3600, "/");
+			header("location: /inc/index.php");
+	}
+	if (isset($_COOKIE['login']) or isset($_COOKIE['username'])) {
 			echo "<span class='txt'>Hello, $username!<br><form action='index.php' method='post'><input type='submit' name='logout' value='Log Out'></form></span>";
 	}
 	if (isset($_POST["logout"])) {
 				setcookie("login", "", time() - 3600, "/");
 				setcookie("username", "", time() - 3600, "/");
-	}
+	}			
 	?>
 
 	<?php
