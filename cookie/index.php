445d4f8587f4852da399c1b48f840aa05ca4887a
darksmetana <darksmetana@gmail.com>
2015-09-17 12:29:41 +0300

    Update index.php

--- cookie/index.php
+++ cookie/index.php
@@ -1,1 +1,24 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>
+		* {
+			margin: 0 auto;
+		}
+		.form {
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<div class="form">
+		<form action="test.php" method="post">
+			Введите Вашу страну:<br>
+			<input type="text" name="country" required><br>
+			<input type="submit" value="Отправить">
+		</form>
+	</div>
+</body>
+</html>
