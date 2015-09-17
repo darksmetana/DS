463045c8cb8436312a020ef40314fe16f85d1e2a
darksmetana <darksmetana@gmail.com>
2015-09-17 13:22:41 +0300

    Update index.php

--- cookie/index.php
+++ cookie/index.php
@@ -19,6 +19,16 @@
 			<input type="text" name="country" required><br>
 			<input type="submit" value="Отправить">
 		</form>
+		<br><br>
+		<form action="test.php" method="post">
+			<input type="submit" name="age" value="Установить куку на 1 час"><br>
+			<input type="submit" name="age1" value="Установить куку на 3 часа"><br>
+			<input type="submit" name="age2" value="Установить куку на день"><br>
+			<input type="submit" name="age3" value="Установить куку на год"><br>
+			<input type="submit" name="age4" value="Установить куку на 10 лет"><br>
+			<input type="submit" name="age5" value="Установить куку до конца текущего дня"><br>
+			<input type="submit" name="age6" value="Установить куку до конца текущего года">
+		</form>
 	</div>
 </body>
 </html>
