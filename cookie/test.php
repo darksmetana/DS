7a587bae853811983cf9ccb753c0663e3f255c07
darksmetana <darksmetana@gmail.com>
2015-09-17 12:29:25 +0300

    Update test.php

--- cookie/test.php
+++ cookie/test.php
@@ -1,1 +1,32 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>
+		* {
+			margin: 0 auto;
+			text-align: center;
+		}
+		form {
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+<?php 
+	if (isset($_POST['country'])) {
+		setcookie('country', $_POST['country']);
+		header("location: test.php");
+	}
+	if (isset($_COOKIE['country'])) {
+		echo "Ваша страна: " . $_COOKIE['country'] . '<br>';
+		echo "<form action='test.php' method='post'><input type='submit' name='delete_cookie' value='Удалить куки'></form>";
+		if (isset($_POST['delete_cookie'])) {
+			setcookie('country', "");
+			header("location: index.php");
+		}
+	}
+?>	
+</body>
+</html>
