298c36b706d1af4ccd2672f19602f8564b505609
darksmetana <darksmetana@gmail.com>
2015-09-18 18:23:20 +0300

    Update index.php

--- cookie/index.php
+++ cookie/index.php
@@ -49,7 +49,87 @@
 			<br>
 			<hr>
 			<br>
+			<?php
+			session_start();
+			?>
+		<style>
+			.wrapper {
+				position: relative;
+				max-width: 980px;
+				padding-top: 90px;
+				overflow: auto;
+			}
+			.product {
+				float: left;
+				width: 32%;
+				margin-top: 15px;
+			}
+			.p_img {
+				width: 200px;
+				height: 200px;
+			}
+			img {
+				width: 100%;
+			}
+			.cart {
+				position: absolute;
+				right: 10px;
+				top: 10px;
+			}
+		</style>
+
 		
+
+	<div class="wrapper">
+	<div class="cart">
+		Корзина: <br>
+		<?php
+			echo $_POST['p_name'];
+		?>
+	</div>
+		
+		<div class="product">
+			<div class="p_img"><img src="/img/p-1.jpg" alt=""></div>
+			<div class="p_price"><?php echo "Цена: " . $price1; ?></div>
+			<form action="index.php" method="post">
+				<input type="hidden" name="p_name" value="Товар 1">Товар 1<br>
+				<input type="submit" name="addcart1" value="Добавить в корзину">
+			</form>
+		</div>
+		<div class="product">
+			<div class="p_img"><img src="/img/p-2.jpg" alt=""></div>
+			<div class="p_price"><?php echo "Цена: " . $price2; ?></div>
+			<form action="index.php" method="post">
+				<input type="hidden" name="p_name" value="Товар 2">Товар 2<br>
+				<input type="submit" name="addcart2" value="Добавить в корзину">
+			</form>
+		</div>
+		<div class="product">
+			<div class="p_img"><img src="/img/p-3.jpg" alt=""></div>
+			<div class="p_price"><?php echo "Цена: " . $price3; ?></div>
+			<form action="index.php" method="post">
+				<input type="hidden" name="p_name" value="Товар 3">Товар 3<br>
+				<input type="submit" name="addcart3" value="Добавить в корзину">
+			</form>
+		</div>
+		<div class="product">
+			<div class="p_img"><img src="/img/p-4.jpg" alt=""></div>
+			<div class="p_price"><?php echo "Цена: " . $price4; ?></div>
+			<form action="index.php" method="post">
+				<input type="hidden" name="p_name" value="Товар 4">Товар 4<br>
+				<input type="submit" name="addcart4" value="Добавить в корзину">
+			</form>
+		</div>
+		<div class="product">
+			<div class="p_img"><img src="/img/p-5.jpg" alt=""></div>
+			<div class="p_price"><?php echo "Цена: " . $price5; ?></div>
+			<form action="index.php" method="post">
+				<input type="hidden" name="p_name" value="Товар 5">Товар 5<br>
+				<input type="submit" name="addcart5" value="Добавить в корзину">
+			</form>
+		</div>
+		
+	</div>
 	</div>
 </body>
 </html>
