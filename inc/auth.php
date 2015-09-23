2c34cda27f2b2bb6f0f5d8cf03296b2f7a2fb78b
darksmetana <darksmetana@gmail.com>
2015-09-23 19:39:05 +0300

    Create auth.php

---
+++ inc/auth.php
@@ -0,0 +1,3 @@
+<?php 
+	echo '<form action="index.php" method="POST" id="log"><input type="text" name="username" required placeholder="Set your name!"><br><input type="text" name="login" required placeholder="Set your login!"><br><input type="submit" name="enter" value="Log In"></form>';
+?>
